86a085661c1b9ed54fd99ac9e0c5dbbe62c2abac
Heechan Kang <weston0713@gmail.com>
2022-08-14 14:44:15 +0900

    challenge done

--- 05_howler/howler.py
+++ 05_howler/howler.py
@@ -25,15 +25,23 @@ def get_args():
 
     parser.add_argument('-o',
                         '--out_file',
-                        help='A named string argument',
+                        help='A name of out_file',
                         metavar='str',
                         type=str,
                         default='')
+    
+    parser.add_argument('-e',
+                        '--ee',
+                        help='A boolean flag',
+                        action='store_true')
 
     args = parser.parse_args()
+    args.dir = False
     
     if os.path.isfile(args.text):
         args.text = open(args.text)
+    elif os.path.isdir(args.text):
+        args.dir = True
     else:
         args.text = io.StringIO(args.text + '\n')
 
@@ -43,13 +51,20 @@ def get_args():
 # --------------------------------------------------
 def main():
     """Make a jazz noise here"""
+
+    def logic(text, out_file, ee=False):
+        out_fh = open(out_file, 'wt') if out_file else sys.stdout
+        for line in text:
+            out_fh.write(line.lower() if ee else line.upper())
+        out_fh.close()
     
     args = get_args()
-    out_fh = open(args.out_file, 'wt') if args.out_file else sys.stdout
-    for line in args.text:
-        out_fh.write(line.upper())
-    out_fh.close()    
-
+    if args.dir:
+        os.mkdir('out-files')
+        for file in os.listdir(args.text):
+            logic(open(os.path.join(args.text, file)).read().strip(), os.path.join('./out-files', file), args.ee)
+    else:
+        logic(args.text, args.out_file, args.ee)
 
 
 # --------------------------------------------------
--- 05_howler/test.py
+++ 05_howler/test.py
@@ -1,8 +1,28 @@
 #!/usr/bin/env python3
 import os
-from subprocess import getstatusoutput
+import re
+import random
+import string
+from subprocess import getstatusoutput, getoutput
+from typing import final
 
 prg = './howler.py'
+
+
+# --------------------------------------------------
+def random_string():
+    """generate a random string"""
+    
+    k = random.randint(5, 10)
+    return ''.join(random.choices(string.ascii_letters + string.digits, k=k))
+
+
+# --------------------------------------------------
+def out_flag():
+    """Either -o of --outfile"""
+    
+    return '-o' if random.randint(0, 1) else '--out_file'
+
 
 # --------------------------------------------------
 def test_exist():
@@ -18,7 +38,7 @@ def test_usage():
     for flag in ['-h', '--help']:
         rv, out = getstatusoutput(f'{prg} {flag}')
         assert rv == 0
-        assert out.lower().startswith('usage')
+        assert re.match('usage', out, re.IGNORECASE)
 
 
 # --------------------------------------------------
@@ -27,17 +47,29 @@ def test_01():
     
     rv, out = getstatusoutput(f'{prg} "Ronald Weasley"')
     assert rv == 0
-    assert out == 'RONALD WEASLEY'
+    assert out.rstrip() == 'RONALD WEASLEY'
 
 
 # --------------------------------------------------
 def test_02():
     """test02"""
     
-    for flag in ['-o', '--out']:
-        rv, out = getstatusoutput(f'{prg} "Ronald Weasley" {flag} ron.txt')
+    out_file = random_string()
+    if os.path.isfile(out_file):
+        os.remove(out_file)
+    
+    
+    try:
+        rv, out = getstatusoutput(f'{prg} "Ronald Weasley" {out_flag()} {out_file}')
         assert rv == 0
-        assert open('ron.txt').read().strip() == 'RONALD WEASLEY'
+        assert out.rstrip() == ''
+        assert os.path.isfile(out_file)
+        text = open('ron.txt').read().rstrip()
+        assert text == 'RONALD WEASLEY'
+    finally:
+        if os.path.isfile(out_file):
+            os.remove(out_file)
+        
 
 # --------------------------------------------------
 def test_03():
@@ -52,7 +84,51 @@ def test_03():
 def test_04():
     """test04"""
     
-    for flag in ['-o', '--out']:
-        rv, out = getstatusoutput(f'{prg} ../inputs/fox.txt {flag} out.txt')
-        assert rv == 0
-        assert open('out.txt').read().strip() == 'THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG.'
+    rv, out = getstatusoutput(f'{prg} ../inputs/fox.txt {out_flag()} out.txt')
+    assert rv == 0
+    assert open('out.txt').read().rstrip() == 'THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG.'
+
+
+# --------------------------------------------------
+def test_file():
+    """Test file in/out"""
+    
+    for expected_file in os.listdir('test-outs'):
+        try:
+            out_file = random_string()
+            if os.path.isfile(out_file):
+                os.remove(out_file)
+            
+            basename = os.path.basename(expected_file)
+            in_file = os.path.join('../inputs', basename)
+            out = getoutput(f'{prg} {out_flag()} {out_file} {in_file}')
+            assert out.strip() == ''
+            produced = open(out_file).read().rstrip()
+            expected = open(os.path.join('test-outs', expected_file)).read().strip()
+            assert produced == expected
+        finally:
+            if os.path.isfile(out_file):
+                os.remove(out_file)
+
+
+# --------------------------------------------------
+def test_06():
+    """test06"""
+
+    try:
+        out_file = random_string()
+        if os.path.isdir('out-files'):
+            os.rmdir('out-files')
+        
+        out = getoutput(f'{prg} {out_flag()} {out_file} "test-outs"')
+        assert out.strip() == ''
+        assert len(os.listdir('test-outs')) == len(os.listdir('out-files'))
+        for test_out in os.listdir('test-outs'):
+            expected = open(os.path.join('test-outs', test_out)).read().strip()
+            produced = open(os.path.join('out-files', test_out)).read().strip()
+            assert produced == expected
+    finally:
+        if os.path.isdir('out-files'):
+            for file in os.listdir('out-files'):
+                os.remove(os.path.join('out-files', file))
+            os.rmdir('out-files')
