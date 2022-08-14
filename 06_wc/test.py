c9b03e77f8fb662f6d6e35166ba57ffa6dc853ae
Heechan Kang <weston0713@gmail.com>
2022-08-14 15:48:47 +0900

    06

---
+++ 06_wc/test.py
@@ -0,0 +1,103 @@
+import os
+import re
+import random
+import string
+from subprocess import getstatusoutput
+
+prg = './wc.py'
+empty = './inputs/empty.txt'
+one_line = './inputs/one.txt'
+two_lines = './inputs/two.txt'
+fox = '../inputs/fox.txt'
+sonnet = '../inputs/sonnet-29.txt'
+
+# --------------------------------------------------
+def test_exist():
+    """exists"""
+    
+    assert os.path.isfile(prg)
+
+
+# --------------------------------------------------
+def test_usage():
+    """usage"""
+    
+    for flag in ['-h', '--help']:
+        rv, out = getstatusoutput(f'{prg} {flag}')
+        assert rv == 0
+        assert re.match('usage', out, re.IGNORECASE)
+
+
+# --------------------------------------------------
+def random_string():
+    """generate a random string"""
+    
+    k = random.randint(5, 10)
+    return ''.join(random.choices(string.ascii_letters + string.digits, k=k))
+
+
+# --------------------------------------------------
+def test_bad_file():
+    """bad file test"""
+    
+    bad = random_string()
+    rv, out = getstatusoutput(f'{prg} {bad}')
+    assert rv != 0
+    assert re.search(f"No such file or directory: '{bad}'", out)
+
+
+# --------------------------------------------------
+def test_empty():
+    """test on empty"""
+    
+    rv, out = getstatusoutput(f'{prg} {empty}')
+    assert rv == 0
+    assert out.rstrip() == '       0       0       0 ./inputs/empty.txt'
+
+
+# --------------------------------------------------
+def test_one():
+    """test on one line"""
+    
+    rv, out = getstatusoutput(f'{prg} {one_line}')
+    assert rv == 0
+    assert out.rstrip() == '       1       1       2 ./inputs/one.txt'
+
+
+# --------------------------------------------------
+def test_two():
+    """test on two lines"""
+    
+    rv, out = getstatusoutput(f'{prg} {two_lines}')
+    assert rv == 0
+    assert out.rstrip() == '       2       2       4 ./inputs/two.txt'
+
+
+# --------------------------------------------------
+def test_fox():
+    """test on fox"""
+    
+    rv, out = getstatusoutput(f'{prg} {fox}')
+    assert rv == 0
+    assert out.rstrip() == '       1       9      45 ../inputs/fox.txt'
+
+
+# --------------------------------------------------
+def test_more():
+    """test on more than one file"""
+    
+    rv, out = getstatusoutput(f'{prg} {fox} {sonnet}')
+    expected = ('       1       9      45 ../inputs/fox.txt\n'
+                '      17     118     661 ../inputs/sonnet-29.txt\n'
+                '      18     127     706 total')
+    assert rv == 0
+    assert out.rstrip() == expected
+
+
+# --------------------------------------------------
+def test_stdin():
+    """test on stdin"""
+    
+    rv, out = getstatusoutput(f'{prg} < {fox}')
+    assert rv == 0
+    assert out.rstrip() == '       1       9      46 <stdin>'
---
+++ 06_wc/wc.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+"""
+Author : Me <me@foo.com>
+Date   : today
+Purpose: Rock the Casbah
+"""
+
+import sys
+import argparse
+
+
+# --------------------------------------------------
+def get_args():
+    """Get command-line arguments"""
+
+    parser = argparse.ArgumentParser(
+        description='Emulate wc (word count)',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument('file',
+                        metavar='FILE',
+                        nargs='*',
+                        default=[sys.stdin],
+                        type=argparse.FileType('rt'),
+                        help='Input file(s)')
+
+
+    return parser.parse_args()
+
+
+# --------------------------------------------------
+def main():
+    """Make a jazz noise here"""
+
+    args = get_args()
+    
+    total_lines, total_bytes, total_words = 0, 0, 0
+    for fh in args.file:
+        num_lines, num_words, num_bytes = 0, 0, 0
+        for line in fh:
+            num_lines += 1
+            num_bytes += len(line)
+            num_words += len(line.split())
+        
+        total_lines += num_lines
+        total_bytes += num_bytes
+        total_words += num_words
+        
+        print(f'{num_lines:8}{num_words:8}{num_bytes:8} {fh.name}')
+    
+    if len(args.file) > 1:
+        print(f'{total_lines:8}{total_words:8}{total_bytes:8} total')
+
+
+# --------------------------------------------------
+if __name__ == '__main__':
+    main()
